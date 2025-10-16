4096c48171a9f25cb09927c604e82e410f44ae3f
jeb <brenopadilha446@gmail.com>
2025-10-16 17:13:51 -0300

    [fix] erros de identação no geral

--- tp4/lista.c
+++ tp4/lista.c
@@ -19,6 +19,7 @@ struct nodo {
 struct lista *lista_cria ()
 {
 	struct lista *l;
+
 	if (!(l = malloc(sizeof(struct lista))))
 		return NULL;
 
@@ -49,6 +50,7 @@ void lista_destroi (struct lista **lista)
 int lista_insere_inicio (struct lista *lista, int chave)
 {
 	struct nodo *novo_nodo;
+
 	if (!(novo_nodo = malloc(sizeof(struct nodo))))
 		return 0;
 
@@ -58,24 +60,23 @@ int lista_insere_inicio (struct lista *lista, int chave)
 	lista->tamanho++;
 	return 1;
 }
-/*botar comentarios */
+
 int lista_insere_fim (struct lista *lista, int chave)
 {
 	struct nodo *novo_nodo, *aux;
-	if (!(novo_nodo = malloc(sizeof(struct nodo))))
-	{
-		return 0;
-	}
 
-	if(!lista_vazia(lista)) /*tem jeito mais elegante de se implementar? */
+	if (!(novo_nodo = malloc(sizeof(struct nodo))))
+		return 0;
+
+	if(!lista_vazia(lista))
 	{
 		aux = lista->ini;
 		while (!(aux->prox == NULL))
-		{ /*ver se resolve o problema do {} */
 			aux = aux->prox;
-		}
+
 		aux->prox = novo_nodo;
 	}
+
 	else
 		lista->ini = novo_nodo;
 
@@ -85,53 +86,45 @@ int lista_insere_fim (struct lista *lista, int chave)
 	return 1;
 }
 
-int lista_insere_ordenado (struct lista *lista, int chave) /*decrescente ou crescente ou ambos? */
+int lista_insere_ordenado (struct lista *lista, int chave)
 {
  	struct nodo *novo_nodo;
 	struct nodo *aux;
+
 	if (!(novo_nodo = malloc(sizeof(struct nodo))))
 		return 0;
 
-	if (lista_vazia(lista)) /* jeito mais elegante? */
-	{
-		lista->ini = novo_nodo;
-		novo_nodo->prox = NULL;
-	}
-
-	else if (lista->ini->chave > chave) /* o que é mais elegante, else if ou double return + definir aux no inicio
-ou usar lista->ini?*/
+	if (lista_vazia(lista) || lista->ini->chave > chave)
 	{
 		novo_nodo->prox = lista->ini;
 		lista->ini = novo_nodo;
 	}
+
 	else
 	{
 		aux = lista->ini;
 		while ((aux->prox != NULL) && (aux->prox->chave < chave))
-		{
 			aux = aux->prox;
-		}
+
 		novo_nodo->prox = aux->prox;
 		aux->prox = novo_nodo;
 	}
 	novo_nodo->chave = chave;
 	lista->tamanho++;
-
 	return 1;
 }
 
 int lista_remove_inicio (struct lista *lista, int *chave)
 {
 	struct nodo *aux;
+
 	if (lista_vazia(lista))
-	return 0;
+		return 0;
 
 	*chave = lista->ini->chave;
-
 	aux = lista->ini;
 	lista->ini = lista->ini->prox;
 	free(aux);
-
 	lista->tamanho--;
 	return 1;
 }
@@ -139,28 +132,28 @@ int lista_remove_inicio (struct lista *lista, int *chave)
 int lista_remove_fim (struct lista *lista, int *chave)
 {
 	struct nodo *aux;
+
 	if (lista_vazia(lista))
-		{
-	return 0;
-		}
+		return 0;
 
 	aux = lista->ini;
-	if ((aux->prox == NULL))
+	if (aux->prox == NULL)
 		{
 			*chave = aux->chave;
 			free(lista->ini);
 			lista->ini = NULL;
 		}
+
 	else
 	{
 		while (!(aux->prox->prox == NULL))
-		{
-		aux = aux->prox;
-		}
+			aux = aux->prox;
+
 		*chave = aux->prox->chave;
 		free(aux->prox);
 		aux->prox = NULL;
 	}
+
 	lista->tamanho--;
 	return 1;
 }
@@ -169,36 +162,27 @@ int lista_remove_ordenado (struct lista *lista, int chave)
 {
 	struct nodo *aux;
 	struct nodo *deleta_nodo;
+
 	if (lista_vazia(lista))
-	{
 		return 0;
-	}
 
 	aux = lista->ini;
-	if (aux->prox == NULL) /* maneira mais eficiente?*/
-	{
-		free(aux);
-		lista->ini = NULL;
-		lista->tamanho--;
-		return 1;
-	}
-	if (aux->chave == chave)
+	if ( lista->ini->prox == NULL || aux->chave == chave)
 	{
  		lista->ini = lista->ini->prox;
-		free(aux); /* unificar os free? deleta_nodo = aux; */
+		deleta_nodo = aux;
 	}
 
 	else
 	{
 		while (aux->prox->chave != chave)
-		{
 			aux = aux->prox;
-		}
+
 		deleta_nodo = aux->prox;
 		aux->prox = aux->prox->prox;
-		free(deleta_nodo);
 	}
 
+	free(deleta_nodo);
 	lista->tamanho--;
 	return 1;
 }
@@ -220,9 +204,8 @@ int lista_pertence (struct lista *lista, int chave)
 	struct nodo *aux = lista->ini;
 
 	while ((aux != NULL) && (aux->chave != chave))
-	{
 		aux = aux->prox;
-	}
+
 	if (aux == NULL)
 		return 0;
 	return 1;
@@ -237,7 +220,7 @@ void lista_inicia_iterador (struct lista *lista)
 int lista_incrementa_iterador (struct lista *lista, int *chave)
 {
 	if (lista->ptr == NULL)
-	return 0;
+		return 0;
 
 	*chave = lista->ptr->chave;
 	lista->ptr = lista->ptr->prox;
